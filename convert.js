5491fc7a8049d998d9a9c4bc7c1cf35fed12bbd0
Mladen Ilic <mladen@stuntcoders.com>
2017-07-27 19:44:52 +0200

    Implement convert script

---
+++ convert.js
@@ -0,0 +1,22 @@
+const ucfirst = function (s) {
+  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+};
+
+module.exports = function (line) {
+  let matches = line.match(/[##|###] (Unreleased):*/i);
+  if (matches && matches.length > 1) {
+    line = `## [${ucfirst(matches[1])}]`;
+  }
+
+  matches = line.match(/[##|###] (ADDED|CHANGED|REMOVED):*/i);
+  if (matches && matches.length > 1) {
+    line = `### ${ucfirst(matches[1])}`;
+  }
+
+  matches = line.match(/[##|###] (\d{1,3}\.\d{1,3}\.\d{1,3}\S*) - (\d{4}\-\d{2}\-\d{2})/i);
+  if (matches && matches.length > 2) {
+    line = `## [${matches[1]}] - ${matches[2]}`;
+  }
+
+  return line;
+}
